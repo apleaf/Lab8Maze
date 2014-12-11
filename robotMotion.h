c799452469c13905dffbe52bf9c5743f28409e8f
apleaf <c16alexander.leaf@usafa.edu>
2014-12-11 14:00:31 -0700

    Create robotMotion.h

---
+++ robotMotion.h
@@ -0,0 +1,21 @@
+
+
+#ifndef ROBOTMOTION_H_
+#define ROBOTMOTION_H_
+
+//#define VAR 5
+
+//funciton declaration
+void initTimerOutputSignals();
+void configureA0andA1Timers();
+void clearModes();
+void turnSmallRight();
+void turnBigRight();
+void turnSmallLeft();
+void turnBigLeft();
+void moveForward();
+void moveBackward();
+void moveSmallBackward();
+
+
+#endif 
