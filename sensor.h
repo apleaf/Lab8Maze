f1b7c262503d6c4806d60a3ff7be94f1b89ca4b2
apleaf <c16alexander.leaf@usafa.edu>
2014-12-11 14:01:35 -0700

    Create sensor.h

---
+++ sensor.h
@@ -0,0 +1,17 @@
+
+
+#ifndef SENSOR_H_
+#define SENSOR_H_
+
+void enableADC10Subsystem();
+void initLEDs();
+unsigned int getLeftSensorReading();
+unsigned int getRightSensorReading();
+unsigned int getCenterSensorReading();
+unsigned char isLeftSensorCloseToWall();
+unsigned char isRightSensorCloseToWall();
+unsigned char isCenterSensorCloseToWall();
+unsigned char isLeftSensorTooFarFromWall();
+unsigned char isLeftSensorTooCloseToWall();
+
+#endif 
